4810481f14700f42093bfdc6ec9873549ee7e506
mhagdorn <magnus.hagdorn@marsupium.org>
2021-12-18 15:29:08 +0000

    fix bug

--- src/CallDipole1D.h
+++ src/CallDipole1D.h
@@ -1,5 +1,5 @@
 extern void c_initialiseDpl1D();
 extern int c_get_nTx();
 extern int c_get_nFreq();
-extern void c_CallDipole1D(int* iTx, int* iFreq);
+extern void c_CallDipole1D(int iTx, int iFreq);
 extern void c_close_outfile();
